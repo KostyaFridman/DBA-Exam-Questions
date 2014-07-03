0f35a4328829b27450aede6614258bab1f6f1ef7
Konstantin-Fridman <Konstantin-Fridman@users.noreply.github.com>
2014-07-03 08:59:36 +0300

    Update Optimistic & Pessimistic concurrencies.sql

--- Optimistic & Pessimistic concurrencies.sql
+++ Optimistic & Pessimistic concurrencies.sql
@@ -7,6 +7,12 @@ http://technet.microsoft.com/en-us/library/aa213031(v=sql.80).aspx
 Optimistic concurrency control uses cursors. 
 Pessimistic concurrency control is the default for SQL Server.
 
-Optimistic concurrency control works on the assumption that resource conflicts between multiple users are unlikely (but not impossible), and allows transactions to execute without locking any resources. Only when attempting to change data are resources checked to determine if any conflicts have occurred. If a conflict occurs, the application must read the data and attempt the change again.
+Optimistic concurrency control works on the assumption that resource conflicts between
+multiple users are unlikely (but not impossible), and allows transactions to execute
+without locking any resources. Only when attempting to change data are resources checked 
+to determine if any conflicts have occurred. If a conflict occurs, the application must 
+read the data and attempt the change again.
 
-Pessimistic concurrency control locks resources as they are required, for the duration of a transaction. Unless deadlocks occur, a transaction is assured of successful completion.
+
+Pessimistic concurrency control locks resources as they are required, for the duration 
+of a transaction. Unless deadlocks occur, a transaction is assured of successful completion.
